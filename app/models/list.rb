c05be127bbc31d89f8163b055dace1786ac45bf3
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2020-04-30 19:30:03 -0300

    Implementing test for Tasks

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,12 @@
 class List < ApplicationRecord
-  has_many :tasks, dependent: :delete_all
   has_many :likes, dependent: :delete_all
+  has_many :tasks, dependent: :delete_all
+  accepts_nested_attributes_for :tasks
 
   belongs_to :user
-  accepts_nested_attributes_for :tasks
+
+  validates :title, length: { minimum: 15 }
+  validates :title, uniqueness: { scope: :user_id }
 
   def first_level_children
     self.tasks.where(parent_id: nil)
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(email: 'test@test.com', password: '1234567890') }
+  let(:params) {
+    {
+        title: 'It is nothing but a test',
+        user: user,
+        tasks_attributes: [
+            { title: 'One Big Task LOL', is_complete: true },
+            { title: 'Another Big Task LOL', is_complete: false }
+        ]
+    }
+  }
+
+  context "When Validating" do
+    before(:each) do
+      List.all.destroy_all
+    end
+
+    it "Valid params generate a new instance" do
+      expect(List.new(params)).to be_valid
+    end
+
+    it "Tasks can be created from this instance" do
+      expect {
+        List.create!(params)
+      }.to change(Task, :count).by(2)
+    end
+
+    it "A new instance won't be created without a user" do
+      params.delete :user
+      expect(
+          List.create(params).errors.messages
+      ).to have_key :user
+    end
+
+    it "A new instance won't be created without a title" do
+      params.delete :title
+      expect(
+          List.create(params).errors.messages
+      ).to have_key :title
+    end
+
+    it "A new instance won't be created with a small title" do
+      params[:title] = "Invalid"
+      expect(
+          List.create(params).errors.messages
+      ).to have_key :title
+    end
+
+    it "A user won't create a new instance with the same title" do
+      List.create!(params)
+      expect(List.new(params)).not_to be_valid
+    end
+  end
+
+  context "Any instance has" do
+    before(:each) do
+      @list = List.new
+    end
+
+    it "a :user method" do
+      expect(@list).to respond_to :user
+    end
+
+    it "a :likes method" do
+      expect(@list).to respond_to :likes
+    end
+
+    it "a :tasks method" do
+      expect(@list).to respond_to :tasks
+    end
+  end
+
+  context "Instance methods" do
+    it "returns the 2 1st level children" do
+      List.all.destroy_all
+      list = List.create!(params)
+      expect(list.first_level_children.length).to eq 2
+    end
+  end
 end
