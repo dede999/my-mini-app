8d7df1877b9d2131f5e52bd4feeec995fe7baae0
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2020-04-30 17:58:19 -0300

    Implementing like's tests
    Signed-off-by: André Luiz Abdalla Silveira <andreluizsa00@gmail.com>

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :list
+
+  validates :user_id, uniqueness: { scope: :list_id }
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(email: 'test@test.com', password: '1234567890') }
+  let(:list) { List.create!(title: 'This is a very special test', user: user) }
+  let(:params) {
+    { user: user, list: list }
+  }
+
+  context 'When validating' do
+    it 'should allow user to follow a list if it\'s not followed yet' do
+      Like.all.destroy_all
+      expect(Like.new(params)).to be_valid
+    end
+
+    it "should not allow a user to follow an already followed list" do
+      Like.all.destroy_all
+      Like.create!(params)
+      expect(Like.new(params)).not_to be_valid
+    end
+
+    it "should not allow a like without a list" do
+      params.delete :list
+      expect(
+          (Like.create(params)).errors.messages
+      ).to have_key :list
+    end
+
+    it "should not allow a like without a user" do
+      params.delete :user
+      expect(
+          (Like.create(params)).errors.messages
+      ).to have_key :user
+    end
+  end
 end
